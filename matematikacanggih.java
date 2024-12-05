d5989a6f63ce7ceda9df4bdb1c56a84f468a6db1
Agustinus-Dintam <187033562+Agustinus-Dintam@users.noreply.github.com>
2024-12-05 16:22:15 +0700

    Add files via upload

---
+++ matematikacanggih.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+ 
+/**
+ *
+ * @author USER
+ */
+public class matematikacanggih {
+    //method tambahan
+    public double operasiTigaAngkah(double a, double b, double c){
+        return a + b - c; // operasi gabungan
+    }
+}
